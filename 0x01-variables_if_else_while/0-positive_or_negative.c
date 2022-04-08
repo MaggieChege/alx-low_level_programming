e9534891c4cac5b8f50b3200804ccddc324ab9d6
Maggie Chege <maggie@hellotractor.com>
2022-04-08 22:39:50 +0300

    variables, if, statements

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Descriptions - prints if random number is +ve, -ve or zero
+ * Return: Returns 0 if code executes successfully
+ */
+
+int main(void)
+{
+    int n;
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+
+    if (n < 0)
+        printf("%d is negative\n", n);
+    else if (n == 0)
+        printf("%d is zeo\n", n);
+    else
+        printf("%d is positive\n", n);
+
+    return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Descriptions - prints if random number is +ve, -ve or zero
+ * Return: Returns 0 if code executes successfully
+ */
+
+int main(void)
+{
+	int n;
+
+    for (n = 0; n <10; n++)
+        putchar(n + '0');
+    putchar('\n');
+    return (0);
+}
