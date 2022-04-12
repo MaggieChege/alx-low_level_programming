a28f2297a5a73e0ee1b14fcf7f062b46313f7bd5
Maggie Chege <maggie@hellotractor.com>
2022-04-12 21:26:32 +0300

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Starting point
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	char str[] = "_putchar";
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
