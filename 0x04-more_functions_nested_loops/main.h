2efdcc2da554a5ed096ae863f1a30fb30b73b3f8
Maggie Chege <maggie@hellotractor.com>
2022-04-14 23:56:54 +0300

    functions and nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef PUTCHAR
 #define PUTCHAR
-#include <stdio.h>
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -13,4 +12,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-#endif /* MAIN_H */
+#endif
